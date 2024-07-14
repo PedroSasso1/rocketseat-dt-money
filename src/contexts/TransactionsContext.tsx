7917ca62a885f8bc97860ae9df7ca118720132e5
pedrosasso1 <pedrosassom@outlook.com>
2024-07-13 23:17:07 -0300

    implement create new transaction form

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,9 +10,12 @@ interface Transaction {
   createdAt: string
 }
 
+type CreateTransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
 interface TransactionContextType {
   transactions: Transaction[]
   fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (input: CreateTransactionInput) => Promise<void>
 }
 
 interface TransactionProviderProps {
@@ -28,9 +31,23 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     const response = await api.get('/transactions', {
       params: {
         q: query,
+        _sort: 'createdAt',
+        _order: 'desc',
       },
     })
     setTransactions(response.data)
+  }
+
+  async function createTransaction(input: CreateTransactionInput) {
+    const response = await api.post('/transactions', {
+      description: input.description,
+      price: input.price,
+      category: input.category,
+      type: input.type,
+      createdAt: new Date(),
+    })
+    const newTransaction = response.data
+    setTransactions((state) => [newTransaction, ...state])
   }
 
   useEffect(() => {
@@ -38,7 +55,9 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransaction }}
+    >
       {children}
     </TransactionsContext.Provider>
   )
